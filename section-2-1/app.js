bcc887c35807c828ad999d8381316000dcc96897
E.STECH <e.s.tech>
2022-07-24 22:28:34 -0300

    Event Modifiers

--- section-2-1/app.js
+++ section-2-1/app.js
@@ -4,12 +4,21 @@ const vm = Vue.createApp({
       firstName: "Joe",
       lastName: "DOE",
       url: "https://google.com",
-      raw_url: '<a href="https://google.com" target="_blank">Google</a>'
+      raw_url: '<a href="https://google.com" target="_blank">Google</a>',
+      age: 20
     }
   },
   methods: {
     fullName() {
       return `${this.firstName} ${this.lastName}`
+    },
+    increment() {
+      this.age++
+    },
+    updateLastName(msg, event) {
+      
+      console.log(msg)
+      this.lastName = event.target.value
     }
   },
 }).mount('#app')
