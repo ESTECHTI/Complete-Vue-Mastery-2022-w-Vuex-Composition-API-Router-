492f0397fcc6ac95417a7c2b648f63e8286fcf74
80836913 <elisandro.oliveira@compasso.com.br>
2022-07-25 14:06:02 -0300

    Watchers

--- section-2-1/app.js
+++ section-2-1/app.js
@@ -2,6 +2,7 @@ const vm = Vue.createApp({
   data() {
     return {
       firstName: "Joe",
+      middleName: "",
       lastName: "DOE",
       url: "https://google.com",
       raw_url: '<a href="https://google.com" target="_blank">Google</a>',
@@ -9,9 +10,6 @@ const vm = Vue.createApp({
     }
   },
   methods: {
-    fullName() {
-      return `${this.firstName} ${this.lastName}`
-    },
     increment() {
       this.age++
     },
@@ -19,6 +17,24 @@ const vm = Vue.createApp({
       
       console.log(msg)
       this.lastName = event.target.value
+    },
+    updateMiddleName(event) {
+      this.middleName = event.target.value
     }
   },
+  computed: {
+    fullName() {
+      console.log('Full name computed property was called!')
+
+      return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
+    }
+
+  },
+  watch: {
+    age(newVal, oldVal) {
+      setTimeout(() => {
+        this.age = 20
+      }, 3000)
+    }
+  }
 }).mount('#app')
