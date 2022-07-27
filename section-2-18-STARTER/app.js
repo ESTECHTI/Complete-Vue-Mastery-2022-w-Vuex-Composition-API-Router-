391c27c6cbbbb17c1f33d3a30a5c3689f8dc81fc
E.STECH <e.s.tech>
2022-07-26 21:45:59 -0300

    Binding classes

--- section-2-18-STARTER/app.js
+++ section-2-18-STARTER/app.js
@@ -2,12 +2,20 @@ let vm = Vue.createApp({
   data() {
     return {
       isPurple: false,
-      selectedColor: ''
+      selectedColor: '',
+      size: 150
     }
   },
   computed: {
     circle_classes() {
       return { purple: this.isPurple }
+    },
+    rotate_circle() {
+      return [
+        { width: this.size + 'px', height: this.size + 'px', lineHeight: this.size + 'px'}, 
+        { transform: 'rotate(30deg)'}
+      ]
     }
+    
   }
 }).mount('#app')
