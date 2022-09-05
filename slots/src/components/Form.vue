2f30aea354eb32a63e7cbacd5dedd7a268549060
80836913 <elisandro.oliveira@compasso.com.br>
2022-09-05 13:05:47 -0300

    Named slots

---
+++ slots/src/components/Form.vue
@@ -0,0 +1,46 @@
+<template>
+ <form>
+  <div class="help">
+    <slot name="help"></slot>
+  </div>
+  <div class="fields">
+    <slot name="fields"></slot>
+  </div>
+  <div class="buttons">
+    <slot name="buttons"></slot>
+  </div>
+  <slot></slot>
+ </form>
+</template>
+ 
+<script>
+ 
+export default {
+  name: 'AppForm',
+  components: {
+ 
+  },
+  props: {
+ 
+  },
+  data:() => {
+    return {
+ 
+    }
+  },
+  computed: {
+ 
+  },
+  methods: {
+ 
+  },
+  mounted() {
+ 
+ },
+ 
+}
+</script>
+ 
+<style lang="scss" scoped>
+ 
+</style>
