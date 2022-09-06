794ccfde2c96eadeb2d8f381bd903030038845d0
80836913 <elisandro.oliveira@compasso.com.br>
2022-09-06 11:02:20 -0300

    Transition CSS Class Names

--- ANIMATIONS/src/App.vue
+++ ANIMATIONS/src/App.vue
@@ -8,17 +8,31 @@
   <!-- <transition name="zoom" type="animation" appear>
     <h2 v-if="flag">hello</h2>
   </transition> -->
-  <transiton
+  <!-- <transition
     @before-enter="beforeEnter"
     @enter="enter"
     @after-enter="afterEnter"
     @before-leave="beforeLeave"
     @leave="leave"
     @after-leave="afterLeave"
-    :css="false"
+    :css="true"
+    name="fade"
   >
     <h2 v-if="flag">Hey</h2>
-  </transiton>
+  </transition> -->
+  <button @click="addItem">Add</button>
+  <ul>
+    <transition-group name="fade"
+      enter-active-class="animate__animated animate__flipInX"
+      leave-active-class="animate__animated animate__flipOutX"
+    >
+      <li v-for="(number, index) in numbers" :key="number"
+        @click="removeItem(index)"
+      >
+        {{ number }}
+      </li>
+    </transition-group>
+    </ul>
  </div>
 </template>
  
@@ -34,26 +48,35 @@ export default {
   },
   data:() => {
     return {
-     flag: true
+     flag: true,
+     numbers: [1, 2, 3, 4, 5, 6, 7]
     }
   },
   computed: {
    
   },
   methods: {
+    addItem() {
+      const num = Math.floor(Math.random() * 100 + 1)
+      const index = Math.floor(Math.random() * this.numbers.length);
+      this.numbers.splice(index, 0, num)
+    },
+    removeItem(index) {
+      this.numbers.splice(index, 1);
+    },
    beforeEnter(el) {
     console.log('before-enter event fired', el)
    },
-   enter(el, done) {
+   enter(el) {
     console.log('enter event fired', el)
 
-    const animation = el.animate([{ transform: "scale3d(0,0,0)" }, {}], {
-      duration: 1000
-    });
+    // const animation = el.animate([{ transform: "scale3d(0,0,0)" }, {}], {
+    //   duration: 1000
+    // });
 
-    animation.onfinish = () => {
-      done();
-    }
+    // animation.onfinish = () => {
+    //   done();
+    // }
    },
    afterEnter(el) {
     console.log('after-enter event fired', el)
@@ -61,16 +84,16 @@ export default {
    beforeLeave(el) {
     console.log('before-leave event fired', el)
    },
-   leave(el, done) {
+   leave(el) {
     console.log('leave event fired', el)
 
-    const animation = el.animate([{}, { transform: "scale3d(0,0,0)" }], {
-      duration: 1000
-    });
+    // const animation = el.animate([{}, { transform: "scale3d(0,0,0)" }], {
+    //   duration: 1000
+    // });
 
-    animation.onfinish = () => {
-      done();
-    }
+    // animation.onfinish = () => {
+    //   done();
+    // }
    },
    afterLeave(el) {
     console.log('after-leave event fired', el)
@@ -84,6 +107,18 @@ export default {
 </script>
  
 <style>
+  .animate__flipOutX {
+    position: absolute;
+  }
+
+  .animate__animated {
+    animation-duration: 1.5s;
+  }
+
+  li {
+    font-size: 22px;
+    cursor: pointer;
+  }
   h2 {
     width: 400px;
     padding: 20px;
@@ -99,6 +134,14 @@ export default {
  .fade-leave-to {
   transition: all 1s linear;
   opacity: 0;
+ }
+
+ .fade-move {
+  transition: all 1s linear;
+ }
+
+ .fade-leave-active {
+  position: absolute
  }
 
  .zoom-enter-active {
