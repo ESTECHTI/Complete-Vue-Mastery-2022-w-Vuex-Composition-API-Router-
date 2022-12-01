f0a5fc84c7e28e0162c286706912fcde11b31cdb
E.STECH <e.s.tech>
2022-12-01 17:34:19 -0300

    restarting the couser

--- vite-project/vite.coonfig.js
+++ vite-project/vite.coonfig.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite" 
 import eslint from "vite-plugin-eslint"
+import vue from "vue-plugin";
 
 export default defineConfig ({
-  plugins: [eslint()]    
+  plugins: [eslint(), vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  }    
 })
