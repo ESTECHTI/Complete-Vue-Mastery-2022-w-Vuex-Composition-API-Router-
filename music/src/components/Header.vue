a9446b134beae575d9b111459f498a71deb9b6cf
E.STECH <e.s.tech>
2022-12-12 11:31:27 -0300

    Guarding Authentication Only Routes

--- music/src/components/Header.vue
+++ music/src/components/Header.vue
@@ -6,22 +6,43 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+      >
         Music
-      </a>
+      </router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
-          <!-- Navigation Links -->
           <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }"
+              >About</router-link
+            >
+          </li>
+          <!-- Navigation Links -->
+          <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click="toggleAuthModal"
               >Login / Register</a
             >
           </li>
-          <li>
-            <a class="px-2 text-white" href="#">Manage</a>
-          </li>
+          <template v-else>
+            <li>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }">
+                Manage
+              </router-link>
+            </li>
+            <li>
+              <a
+                class="px-2 text-white"
+                href="#"
+                @click.prevent="userStore.signOut"
+                >Logout</a
+              >
+            </li>
+          </template>
         </ul>
       </div>
     </nav>
@@ -31,13 +52,14 @@
 <script>
 import { mapStores } from "pinia";
 import useModalStore from "../stores/modal";
+import userStore from "@/stores/user";
 
 export default {
   data: () => {
     return {};
   },
   computed: {
-    ...mapStores(useModalStore),
+    ...mapStores(useModalStore, userStore),
   },
   methods: {
     toggleAuthModal() {
--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -110,7 +110,8 @@
 </template>
 
 <script>
-import { auth, usersCollection } from "../includes/firebase";
+import { mapActions } from "pinia";
+import useUserStore from "@/stores/user";
 
 export default {
   name: "RegisterForm",
@@ -137,33 +138,17 @@ export default {
     };
   },
   methods: {
+    ...mapActions(useUserStore, {
+      createUser: "register",
+    }),
     async registerForm(values) {
       this.reg_show_alert = true;
       this.reg_in_submission = true;
       this.reg_alert_variant = "bg-blue-500";
       this.reg_alert_msg = "Please wait! Your account is being created.";
 
-      let userCred = null;
       try {
-        userCred = await auth().createUserWithEmailAndPassword(
-          values.email,
-          values.password
-        );
-      } catch (error) {
-        this.reg_in_submission = false;
-        this.reg_alert_variant = "bg-red-500";
-        this.reg_alert_msg =
-          "An unexpected error occurred. Please try again later";
-        return;
-      }
-
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country,
-        });
+        await this.createUser(values);
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = "bg-red-500";
@@ -174,7 +159,7 @@ export default {
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Success! Your account is being created.";
-      console.log("userCred", userCred);
+      window.location.reload();
     },
   },
 };
