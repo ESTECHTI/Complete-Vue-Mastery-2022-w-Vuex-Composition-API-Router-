4a71b1bf7cf629233d14a368bb6bf265d19ff9fe
ESTECHTI <e.s.tech@outlook.com>
2023-01-03 09:18:09 -0300

    Route Leave Guards

--- music/src/components/CompositionItem.vue
+++ music/src/components/CompositionItem.vue
@@ -34,6 +34,7 @@
             type="text"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Song Title"
+            @input="updateUnsavedFlag(true)"
           />
           <ErrorMessage class="text-red-600" name="modified_name"/>
         </div>
@@ -44,6 +45,7 @@
             type="text"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Genre"
+            @input="updateUnsavedFlag(true)"
           />
           <ErrorMessage class="text-red-600" name="genre"/>
         </div>
@@ -88,6 +90,9 @@ export default {
     removeSong: {
       type: Function,
       required: true,
+    },
+    updateUnsavedFlag: {
+      type: Function,
     }
   },
   components: {},
@@ -122,6 +127,7 @@ export default {
       }
 
       this.updateSong(this.index, values);
+      this.updateUnsavedFlag(false);
 
       this.in_submission = false;
       this.alert_variant = 'bg-green-500',
